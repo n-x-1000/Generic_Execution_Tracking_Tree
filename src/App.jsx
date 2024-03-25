7b3b297227d1dfbbd2ac0ff0ab4cbfcb6cb65f79
nakulx1000 <nakulx1000@gmail.com>
2024-03-25 09:41:49 +0530

    Tree commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tree from './tree'; // Import the Tree component
+
+function App() {
+  return (
+    <div>
+      <h1>Collapsible Tree</h1>
+      <Tree />
+    </div>
+  );
+}
+
+export default App;
