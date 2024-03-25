fe9871b25b462a1deffc67aadc533a5010ba90e3
nakulx1000 <nakulx1000@gmail.com>
2024-03-25 10:54:24 +0530

    Minor fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dist/',
   build: {
-    outDir: 'dist', // Specify the output directory here
+    outDir: 'dist/', // Specify the output directory here
   },
 })
